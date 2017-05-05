ee27d340a1e283867a8527188c21d1b6ac06af88
JanVana <jan.vana@outlook.com>
2017-05-05 09:29:16 +0200

    Tomcat conflicting file

--- configuration/tomcat/tomcat-setenv.sh
+++ configuration/tomcat/tomcat-setenv.sh
@@ -3,3 +3,4 @@
 export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m"
 export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m"
 export JAVA_OPTS="${JAVA_OPTS} -Dcz.cuni.mff.xrg.odalic.sti=/usr/local/tomcat/conf/odalic/sti.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Dorg.apache.catalina.startup.ContextConfig.jarsToSkip=bcprov-jdk15on-1.55.jar"
