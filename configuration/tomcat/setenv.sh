5ce6126552f02b22c81fbf6898d9fe31a0edc842
JanVana <jan.vana@outlook.com>
2017-05-05 09:58:57 +0200

    Tomcat configuration fix

---
+++ configuration/tomcat/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export CATALINA_OPTS="$CATALINA_OPTS -Xms1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx2048m"
+export JAVA_OPTS="${JAVA_OPTS} -Dcz.cuni.mff.xrg.odalic.sti=/usr/local/tomcat/conf/odalic/sti.properties"
